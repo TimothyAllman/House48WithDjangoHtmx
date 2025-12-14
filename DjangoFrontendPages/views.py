d88f2d641a73e19d0e92d93aa5fbf55ff53c0491
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-12-14 17:44:34 +0200

    Update views.py

--- DjangoFrontendPages/views.py
+++ DjangoFrontendPages/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import RedirectView
 
 # Create your views here.
+def NoPageEndpoint(request):
+    return RedirectView.as_view(url="home/", permanent=False)
+
 def HomePageEndpoint(request):
     return render(request,"HomePage.html")
 
-def OtherPageEndpoint(request):
-    return render(request,"OtherPage.html")
+def OtherPagesEndpoint(request):
+    return RedirectView.as_view(url="otherList/", permanent=False)
+
+def OtherListPageEndpoint(request):
+    return render(request,"OtherListPage.html")
 
 def TallPageEndpoint(request):
     return render(request,"TallPage.html")
